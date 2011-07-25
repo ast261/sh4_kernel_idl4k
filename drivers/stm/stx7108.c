17693ca7fc9f6636e282b4ac4f3de389765203ec
Giuseppe Cavallaro <peppe.cavallaro@st.com>
2011-07-25 15:50:05 +0200

    stm_stx7108: add the wl12xx platform information
    Enabling the CONFIG_WL12XX_PLATFORM_DATA the hdk7108
    will be able to use the TI WL1271 SDIO Wireless card
    plugged into the MMC slot.
    Selecting this option the MMC slot won't be used for
    managing SD and MMC cards due to the DATA4 line used
    for providing the SDIO IRQ instead of MMC data[4].
    
    This patch passes the platform information used for
    configuring the SDIO TI driver provided by third parts
    (for example compact-wireless sources).
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>

--- drivers/stm/stx7108.c
+++ drivers/stm/stx7108.c
@@ -21,10 +21,11 @@
 #include <linux/stm/device.h>
 #include <linux/stm/sysconf.h>
 #include <linux/stm/stx7108.h>
+#ifdef CONFIG_WL12XX_PLATFORM_DATA
+#include <linux/wl12xx.h>
+#endif
 #include <asm/irq-ilc.h>
 #include "pio-control.h"
-
-
 /* EMI resources ---------------------------------------------------------- */
 
 static int __initdata stx7108_emi_bank_configured[EMI_BANKS];
@@ -816,7 +817,11 @@ static struct stm_pad_config stx7108_mmc_pad_config = {
 		STX7108_PIO_MMC_BIDIR(0, 1),	/* MMC Data[1]*/
 		STX7108_PIO_MMC_BIDIR(0, 2),	/* MMC Data[2]*/
 		STX7108_PIO_MMC_BIDIR(0, 3),	/* MMC Data[3]*/
+#ifndef CONFIG_WL12XX_PLATFORM_DATA
 		STX7108_PIO_MMC_BIDIR(0, 4),	/* MMC Data[4]*/
+#else
+		STX7108_PIO_MMC_IN(0, 4),	/* SDIO WLAN_IRQ*/
+#endif
 		STX7108_PIO_MMC_BIDIR(0, 5),	/* MMC Data[5]*/
 		STX7108_PIO_MMC_BIDIR(0, 6),	/* MMC Data[6]*/
 		STX7108_PIO_MMC_BIDIR(0, 7),	/* MMC Data[7]*/
@@ -853,6 +858,12 @@ static struct platform_device stx7108_mmc_device = {
 
 #define PIO1_CFG_CLKNODATA	0x100
 
+#ifdef CONFIG_WL12XX_PLATFORM_DATA
+struct wl12xx_platform_data stx7108_wl12xx_plt_data __initdata = {
+	.board_ref_clock = WL12XX_REFCLOCK_38,
+};
+#endif
+
 void __init stx7108_configure_mmc(int emmc)
 {
 	struct sdhci_pltfm_data *plat_data;
@@ -863,6 +874,23 @@ void __init stx7108_configure_mmc(int emmc)
 		plat_data->quirks |= SDHCI_QUIRK_NONREMOVABLE_CARD;
 
 	platform_device_register(&stx7108_mmc_device);
+
+#ifdef CONFIG_WL12XX_PLATFORM_DATA
+	/* Switch the MMC HC to work in PIO mode. */
+	plat_data->quirks |= SDHCI_QUIRK_BROKEN_DMA |
+			     SDHCI_QUIRK_BROKEN_ADMA |
+			     SDHCI_QUIRK_FORCE_1_BIT_DATA;
+
+	/* PIO0[4] (MMC DATA4) is the SDIO irq and it's configured
+	 * as input.
+	 *
+	 * **** Selecting this driver the MMC cannot work on
+	 *      other MMC devices (e.g. SD/MMC cards).
+	 */
+	stx7108_wl12xx_plt_data.irq = gpio_to_irq(stm_gpio(0, 4));
+	if (wl12xx_set_platform_data(&stx7108_wl12xx_plt_data))
+		pr_err("%s: ERROR setting wl12xx data\n", __func__);
+#endif
 }
 
 /* sysconf resources ------------------------------------------------------ */
